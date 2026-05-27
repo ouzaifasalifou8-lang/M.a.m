96afc25d08e40f5ae8306ea5e74fc91109f6c27d
ouzaifasalifou8-lang <ouzaifasalifou8@gmail.com>
2026-05-27 18:14:29 +0100

    Delete js directory

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-// Initialisation correcte du client Supabase
-const SUPABASE_URL = 'https://dzbomxdmnznzykppbguh.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_09bNEz6hpAH_QeD91-dhZw_ufVBhDlS'; // REMPLACEZ PAR VOTRE VRAIE CLÉ
-
-// On crée le client de manière globale
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
