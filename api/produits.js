0935667a69a67263bb01d70284d2e90ff84c8285
ouzaifasalifou8-lang <ouzaifasalifou8@gmail.com>
2026-05-28 00:29:33 +0100

    Update produits.js

--- api/produits.js
+++ api/produits.js
@@ -1,17 +1,19 @@
 // api/produits.js
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://Ouzaif:WwmcCM6p5XaLYVx0@cluster0.4amt8zx.mongodb.net/?appName=Cluster0";
+import { createClient } from "@libsql/client";
 
 export default async function handler(req, res) {
-  const client = new MongoClient(uri);
-  await client.connect();
-  const db = client.db('MAMshopDB');
-  
+  // Initialisation du client Turso avec vos clés
+  const db = createClient({
+    url: "libsql://mam-ouzaif.aws-eu-west-1.turso.io",
+    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3Nzk5MjQ0MzksImlkIjoiMDE5ZTZiYmYtN2IwMS03ODAwLTgxMjAtMmIxYWU3ZmY5NDhiIiwicmlkIjoiNTc5N2IxMTMtYWJkNS00NzNmLWE0ZDQtMjg3NDczZmYxOTFiIn0.6Tyuoc9kZP26Jigx_Vt8icENLzbUGCfmQeiFGd9tgpcdl95X_Imfxxkj9L6e-vkVgTO4QA7hmtSzyFryxUiLBg"
+  });
+
   if (req.method === 'POST') {
-    const result = await db.collection('Produits').insertOne(req.body);
-    res.status(200).json(result);
+    const { nom } = req.body;
+    await db.execute("INSERT INTO Produits (nom) VALUES (?)", [nom]);
+    res.status(200).json({ message: "Produit ajouté" });
   } else {
-    const produits = await db.collection('Produits').find({}).toArray();
-    res.status(200).json(produits);
+    const result = await db.execute("SELECT * FROM Produits");
+    res.status(200).json(result.rows);
   }
 }
